6e76b8c1d00f42e245bd33ccef132a30ceaea8f1
Juanmicheljm65 <juanmicheljm65@gmail.com>
2024-04-03 21:14:34 -0300

    Sistema simulando um banco

---
+++ banco.py
@@ -0,0 +1,42 @@
+menu = '''
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Sair
+
+'''
+saldo = 0
+saques = 0
+VALOR_LIMITE = 500
+LIMITE_DE_SAQUE = 3
+extrato = ""
+
+while True :
+    opcao = input(menu)
+    
+    if opcao == '1' :
+        deposito = float(input('\nInforme o valor que deseja depositar: '))
+        saldo += deposito
+        extrato += (f'\nDepositou R${float(deposito):.2f}')
+        print(f'\nSeu deposito de R${float(deposito):.2f} foi concluído. Voltando ao menu...')
+    
+    elif opcao == '2' :
+        if saques  <= LIMITE_DE_SAQUE:
+            sacar = float(input(f'\nSeu saldo atual é: R${float(saldo):.2f}. Quanto gostaria de sacar? '))
+            if sacar <= saldo and sacar <= VALOR_LIMITE and sacar != 0 : 
+                saldo -= sacar
+                extrato += (f'\nSacou R${float(sacar):.2f}')
+                saques += 1
+                print(f'\nSaque realizado com sucesso! Seu saldo restante é de: R${float(saldo):.2f} \nVoltando ao menu...')
+            else :
+                print('\nSaldo insuficiente, valor nulo ou o valor de saque maior que R$500! \nVoltando ao menu...')
+        else :
+            print('\nVocê já atingiu o limite de saque diário. \nVoltando ao menu...')
+        
+    elif opcao == '3' :
+        print(f'\nSeu extrato:\n{extrato.lstrip()} \nSeu saldo atual é de: R${float(saldo):.2f}. \nVocê tem {3 - saques} saques restantes hoje. \nVoltando ao menu...')
+    
+    else :
+        print('\nSaindo...')
+        break
